8e309eff9f6aaa8e40bb1174abff457c363e966f
Vera Thornbury <vcthornbury@gmail.com>
2021-04-10 14:05:05 -0400

    Frontend Base service methods created for event/invite get/creation/updates. Minor edits in backend Event controller.

--- final-capstone/vue/src/services/BaseService.js
+++ final-capstone/vue/src/services/BaseService.js
@@ -14,6 +14,44 @@ export default {
     //this needs wired up properly
     searchLocationCity(city){
         return axios.get(`/search?city=${city}`)
+    },
+
+    //get all users
+    getAllUsers(){
+        return axios.get(`/users/list`)
+    },
+
+    //get event by event id
+    getEventById(eventId){
+        return axios.get(`/event/${eventId}`)
+    },
+
+    //get invite by invite id
+    getInviteByInviteId(inviteId){
+        return axios.get(`/event/invite/${inviteId}`)
+    },
+
+    //get invites by event id
+    getInvitesByEventId(eventId){
+        return axios.get(`/event/${eventId}/invites`)
+    },
+    // get invites by user id
+    getInvitesByUserId(userId){
+        return axios.get(`/users/${userId}/invites`)
+    },
+
+    //create event
+    createEvent(event){
+        return axios.post( `/event`, event)
+    },
+    //create invite
+    createInvite(invite){
+        return axios.post(`/event/invite`, invite)
+    },
+
+    //update invite with votes
+    updateVotes(invite){
+        return axios.put(`/event/invite/${invite.inviteId}`, invite)
     }
         
 }
