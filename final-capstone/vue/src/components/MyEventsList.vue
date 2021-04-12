669500ea1951f527fb44c2a43218fecdeb8356cc
Connor <78218173+Hjoin@users.noreply.github.com>
2021-04-12 15:07:55 -0400

    Started creating a component for MyEvents and a view for MyEvents.

---
+++ final-capstone/vue/src/components/MyEventsList.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  <br> <!--- need to add css to replace br -->
+  <br>
+  <br>
+    <h2>My Events:</h2>
+    <p>
+        {{user.myEvents}}
+    </p>
+<!---adding the user's events --->
+  
+</div>
+</template>
+
+<script>
+
+import BaseService from "../services/BaseService"
+export default {
+    data(){
+        return{
+            eventId: "",
+            users:[],
+            invited:[]
+        }
+    },
+    created(){
+      BaseService.getEventById(eventId).then((response) => {
+          this.users = response.data;
+      });
+    }
+}
+</script>
+
+<style>
+
+</style>
