38144b6371e96c6c8ebd039239b081b1eb30ee7d
Austin Kinnard <austink145@gmail.com>
2021-04-14 15:05:32 -0400

    added results component to view results

---
+++ final-capstone/vue/src/components/Results.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <br />
+    <br />
+    <br />
+
+    <div v-for="(invite, index) in invitation" v-bind:key="invite.id">
+      <button v-on:click="getRestaurants(index), temp=index" v-bind="temp" >{{ index + 1 }}</button>
+      <!-- <button v-on:click="location.reload()">X</button> -->
+      <button v-on:click="BaseService.updateVotes(this.invitation[index])">Submit</button>
+    </div>
+</template>
+
+<script>
+import BaseService from "../services/BaseService";
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
